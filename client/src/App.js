73234d560c8e8f43c126dc09a91a0bf98f538243
Tjain-Web-developer <tanishqjain2114@gmail.com>
2023-02-11 09:42:51 +0530

    Hackathon Second Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from './Components/Home/Home'
+import Register from './Components/Register/Register'
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/register' element={<Register/>} />
       </Routes>
     </BrowserRouter>
   )

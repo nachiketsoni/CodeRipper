f683f506b52af4f17574095b4d17985dda3c8b39
Pawan <pawans6343@gmail.com>
2023-02-12 08:13:37 +0530

    added home page ui

--- Server/controllers/indexController.js
+++ Server/controllers/indexController.js
@@ -7,47 +7,47 @@ const ErrorHandler = require("../utils/errorHandler.js");
 const AsyncError = require("../middleware/asyncErrors");
 
 exports.Homepage = (req, res) => {
-    res.status(200).json("Success!")
-}
-
+  res.status(200).json("Success!");
+};
 
 /**@api POST / login */
 exports.login = AsyncError(async (req, res, next) => {
-    const { email, password } = req.body;
-  
-    // checking if user has given password and email both
-  
-    if (!email || !password) {
-      return next(new ErrorHandler("Please Enter Email & Password", 400));
-    }
-  
-    const user = await User.findOne({ email }).select("+password");
-  
-    if (!user) {
-      return next(new ErrorHandler("Invalid email or password", 401));
-    }
-  
-    const isPasswordMatched = await user.comparePassword(password);
-  
-    if (!isPasswordMatched) {
-      return next(new ErrorHandler("Invalid email or password", 401));
-    }
-  
-    sendToken(res, 200, user, next);
-  });
+  const { email, password } = req.body;
 
-  /**@api POST / signup */
-exports.signup = (async (req, res, next) => {
-    try {
-      const { name, email, password } = req.body;
+  // checking if user has given password and email both
+
+  if (!email || !password) {
+    return next(new ErrorHandler("Please Enter Email & Password", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  sendToken(res, 200, user, next);
+});
+
+/**@api POST / signup */
+exports.signup = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
     // res.json(req.body)
     const defaultIMG =
       "https://img.freepik.com/free-psd/3d-illustration-person_23-2149436192.jpg?w=740&t=st=1665479565~exp=1665480165~hmac=a506127a19be062f341ab4d2e9767e3a1593d6e20efd3762ebfcb19cc39e49d1  ";
     if (!email || !password || !name) {
-      return next(new ErrorHandler("Please Enter Name , Email & Password", 400));
+      return next(
+        new ErrorHandler("Please Enter Name , Email & Password", 400)
+      );
     }
-    try{
-      
+    try {
       var { public_id, secure_url } = await cloudinary.v2.uploader.upload(
         defaultIMG,
         {
@@ -56,11 +56,10 @@ exports.signup = (async (req, res, next) => {
           quality: "50",
         }
       );
-    }catch(err){
-      console.log(err)
+    } catch (err) {
+      console.log(err);
     }
 
-  
     const user = new User({
       name,
       email,
@@ -72,67 +71,68 @@ exports.signup = (async (req, res, next) => {
     });
     const createdUser = await user.save();
     sendToken(res, 200, createdUser);
-    } catch (error) {
-      res.status(500).json({success: false, message: error.message });
-    }
-  });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
-  /**@api GET / logout */
+/**@api GET / logout */
 exports.logout = AsyncError((req, res, next) => {
   res.clearCookie("token");
   res.status(200).json({ message: "user logged out" });
 });
 
 /**@api POST / update */
-exports.update = (async (req, res, next) => {
+exports.update = async (req, res, next) => {
   try {
     const data = req.body;
-    const user = await User.findByIdAndUpdate(req.params.id , data, {
-      new: true} )
+    const user = await User.findByIdAndUpdate(req.params.id, data, {
+      new: true,
+    });
     await user.save();
     res.status(200).json({ success: true, user });
   } catch (error) {
-    res.status(500).json({success: false, message: error.message });
+    res.status(500).json({ success: false, message: error.message });
   }
-});
+};
 
 /**
  * @api GET / profile of user
  */
-exports.getSingleUser = (async (req, res, next) =>  {
+exports.getSingleUser = async (req, res, next) => {
   const user = await User.findById(req.params.id);
   res.status(200).json({ success: true, user });
-});
+};
 
 /**
  * @api GET / get all users
  */
-exports.getAllUsers  = (async (req, res, next) =>  {      
+exports.getAllUsers = async (req, res, next) => {
   try {
     const users = await User.find();
     res.status(200).json({ success: true, users });
   } catch (error) {
-    res.status(500).json({success: false, message: error.message });
+    res.status(500).json({ success: false, message: error.message });
   }
-});
+};
 
-exports.getLoggedInUser = (async (req, res, next) =>  {   
+exports.getLoggedInUser = async (req, res, next) => {
   try {
     const user = await User.findById(req.user._id);
     res.status(200).json({ success: true, user });
   } catch (error) {
-    res.status(500).json({success: false, message: error.message });
+    res.status(500).json({ success: false, message: error.message });
   }
-});
+};
 
-exports.getMyGeneratedWaste = (async (req, res, next) =>  {
+exports.getMyGeneratedWaste = async (req, res, next) => {
   try {
     const user = await User.findById(req.user._id).populate("request");
     res.status(200).json({ success: true, user });
   } catch (error) {
-    res.status(500).json({success: false, message: error.message });
+    res.status(500).json({ success: false, message: error.message });
   }
-});
+};
 
 /** @api POST / send otp to phone number */
 
@@ -140,15 +140,17 @@ exports.sendOTP = AsyncError(async (req, res, next) => {
   const accountSid = process.env.TWILIO_ACCOUNT_SID;
   const authToken = process.env.TWILIO_AUTH_TOKEN;
   const client = require("twilio")(accountSid, authToken);
+  
   const OTP = Math.floor(1000 + Math.random() * 9000);
 
   client.messages
     .create({
       body: `OTP for verifying the request is ${OTP}`,
-      to: `${req.body.countryCode}${req.body.number}`, // Text this number
+      to: `+91${req.body.number}`, // Text this number
       from: process.env.TWILIO_PHONE_NUMBER, // From a valid Twilio number
     })
-    .then((message) =>
-      res.json(201).json({ message: "OTP sent successfully" })
-    );
+    .then((message) => res.json(201).json({ message: "OTP sent successfully" }))
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
-import React from 'react'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import Home from './Components/Home/Home'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home/Home";
 
-import Service from './Components/Service/service'
-import Pickup from './Components/Pickup/Pickup'
+import Service from "./Components/Service/service";
+import Pickup from "./Components/Pickup/Pickup";
 
-import Register from './Components/Register/Register'
+import Register from "./Components/Register/Register";
+import Navbar from "./Components/Home/Navbar";
 
 const App = () => {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/register' element={<Register/>} />
-        </Routes>
-      </BrowserRouter>
-  )
-}
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;

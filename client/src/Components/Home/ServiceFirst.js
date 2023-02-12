9d34b99594bdfadb7e299759d66a2d9847d26d42
Pawan <pawans6343@gmail.com>
2023-02-12 12:47:59 +0530

    ui added , home page, services,subscription.

---
+++ client/src/Components/Home/ServiceFirst.js
@@ -0,0 +1,31 @@
+import React from "react";
+import css from "./Services.module.css";
+import serviceIllustration1 from "../../assets/image 5.png";
+const Services = () => {
+  return (
+    <div
+      className={`${css.wrapper} d-flex justify-content-center align-items-center me-5`}
+    >
+      <div className={css.imageHolder}>
+        <img className={css.illustration} src={serviceIllustration1}></img>
+      </div>
+
+      <div>
+        <div className={`${css.right} w-75`}>
+          <div>
+            <h1 className={`${css.textTop}`}>
+              Quick Pickup <br />
+              Service
+            </h1>
+            <h5 className={`${css.textMid}`}>
+              Schedule a Quick Pickup through <br /> a single call of your phone
+            </h5>
+            <button className={`${css.button}`}> Schedule Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;

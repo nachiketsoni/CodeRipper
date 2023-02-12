a3b74e01e9bb6e11b2e7be07b0911390ea3fc195
Tjain-Web-developer <tanishqjain2114@gmail.com>
2023-02-12 08:27:34 +0530

    Integrated Backend and Frontend

--- client/src/Store/UserSlice/Userslice.js
+++ client/src/Store/UserSlice/Userslice.js
@@ -30,3 +30,41 @@ export const userSlice = createSlice({
 
 // Action creators are generated for each case reducer function
 export const { login, errors, logout } = userSlice.actions;
+
+export const loginAsync = (dets) => async (dispatch, getState) => {
+    try {
+        const { data } = await axios.post("/login", dets);
+        dispatch(login(data));
+    } catch (error) {
+        dispatch(errors(error.response.data.error));
+    }
+};
+
+export const signUpAsync = (dets) => async (dispatch, getState) => {
+    try {
+        const { data } = await axios.post("/signup", dets);
+        dispatch(login(data));
+    } catch (error) {
+        dispatch(errors(error.response.data.error));
+    }
+};
+
+export const loadUserAsync = () => async (dispatch, getState) => {
+    try {
+        const { data } = await axios.get("/getLoggedInUser");
+        dispatch(login(data));
+    } catch (error) {
+        dispatch(errors(error.response.data.message));
+    }
+};
+
+export const logoutAsync = () => async (dispatch, getState) => {
+    try {
+        await axios.get("/logout");
+        dispatch(logout());
+    } catch (error) {
+        dispatch(errors(error.response.data.error));
+    }
+};
+
+export default userSlice.reducer;
